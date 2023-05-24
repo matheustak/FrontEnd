4f1a48084271c5d40f43ce19f9f8163e0c3fca59
matheus <129976192+matheustak@users.noreply.github.com>
2023-05-24 19:33:36 -0400

    movendo projeto

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/**
+ * @author matheus
+ *
+ */
+public class OrderItemPK {
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	
+	
+	@ManyToOne
+	@JoinColumn(name =	"product_id")
+	private Product product;
+	
+	public OrderItemPK() {}
+
+	
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
+	
+
+}

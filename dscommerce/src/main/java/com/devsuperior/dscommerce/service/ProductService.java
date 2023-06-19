9dc7669de19adea6a9d23c01542ab38f0562ca7a
matheus <129976192+matheustak@users.noreply.github.com>
2023-06-19 19:24:51 -0400

    page
    busca paginada

--- dscommerce/src/main/java/com/devsuperior/dscommerce/service/ProductService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/service/ProductService.java
@@ -3,9 +3,12 @@
  */
 package com.devsuperior.dscommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,5 +37,14 @@ public class ProductService {
 		return dto;
 		
 	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll( Pageable pageable){
+		Page<Product> result = repository.findAll(pageable);
+		
+		return result.map(x -> new ProductDTO(x));
+	}
+	
+	
 
 }
